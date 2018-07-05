0edf5e6eb1879555f146ad1b68b4041549deecfd
Abhishek Rai <arai16@sapient.com>
2018-07-05 18:11:27 +0100

    Test script

---
+++ test.js
@@ -0,0 +1,32 @@
+var possiblePaths = 0;
+var x = 4;
+var stepsArr = [1,2];
+var maxPossiblility = stepsArr.length * x;
+var next = 0;
+
+for(i=0;i<maxPossiblility;i++){
+
+    var step=0;
+    if(stepsArr[i]<=x){
+        for(y=0;y<x;y++){
+            console.log(step, ' + ', stepsArr[i])
+            if(x-step >= stepsArr[i]){
+                step = step + stepsArr[next];
+            }
+            console.log('Result: ', step);
+
+            if(step == x){
+                possiblePaths++;
+                //console.log('bbbbbbb: ', possiblePaths);
+                next++;
+                step=0;
+            }
+
+        }
+    } else{
+        console.log('Steps are should be more than higest stair');
+    }
+}
+console.log('possiblePaths', possiblePaths);
+
+   
